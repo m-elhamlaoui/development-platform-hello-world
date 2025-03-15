afb6c2bd2752497b0da2a18ebdb6311080bbfa5b
Ayman ANNA <ayman.anna111@gmail.com>
2025-03-15 00:51:01 +0000

    Setup of Spring microservice App

---
+++ SpaceApp/SpringBackend/CollisionService/src/test/java/org/example/collisionservice/CollisionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.collisionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollisionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SpaceApp/SpringBackend/EndOfLifeService/src/main/java/org/example/endoflifeservice/EndOfLifeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.endoflifeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndOfLifeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndOfLifeServiceApplication.class, args);
+    }
+
+}
