a80efaaa119746d936fa3f7e8f043134a445f9fe
Ismail-Ah <ahakayismail4@gmail.com>
2025-04-09 22:07:39 +0100

    setup django

---
+++ SpaceApp/SatelliteTracker/eol_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EolServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'eol_service'
---
+++ SpaceApp/SatelliteTracker/kafka_config/kafka_setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'Kafka setup placeholder'
