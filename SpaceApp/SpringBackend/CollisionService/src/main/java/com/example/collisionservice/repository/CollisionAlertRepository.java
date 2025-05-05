d4fe4da92d998285b53d43c329109eede84d12f6
Ismail-Ah <ahakayismail4@gmail.com>
2025-05-06 00:48:35 +0100

    link front with collisionservice

--- SpaceApp/SpringBackend/CollisionService/src/main/java/com/example/collisionservice/repository/CollisionAlertRepository.java
+++ SpaceApp/SpringBackend/CollisionService/src/main/java/com/example/collisionservice/repository/CollisionAlertRepository.java
@@ -1,7 +1,13 @@
 package com.example.collisionservice.repository;
 
-import com.example.collisionservice.model.CollisionAlert;
+import com.example.collisionservice.model.CollisionAlertModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CollisionAlertRepository extends MongoRepository<CollisionAlert, String> {
+import java.util.List;
+
+@Repository
+public interface CollisionAlertRepository extends MongoRepository<CollisionAlertModel, String> {
+    List<CollisionAlertModel> findByDangerLevel(String dangerLevel);
+    List<CollisionAlertModel> findBySatellitesContaining(String satelliteId);
 } 
