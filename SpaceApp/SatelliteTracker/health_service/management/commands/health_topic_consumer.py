ca16c599a3e5039bc2ea83b06576cc85b6495a71
youssefbouraoui1 <bouraouiyoussef12@gmail.com>
2025-05-09 23:09:21 +0100

    everything is good

--- SpaceApp/SatelliteTracker/health_service/management/commands/health_topic_consumer.py
+++ SpaceApp/SatelliteTracker/health_service/management/commands/health_topic_consumer.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
+from health_service.predicting_health import start_health_data_consumer
 
 
 class Command(BaseCommand):
     help = "start the health consumer"
 
     def handle(self, *args, **options):
-        return super().handle(*args, **options)
+        start_health_data_consumer()
