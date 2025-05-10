5109440fe2b8e37f57c830f8c399e3dc63cac5e1
youssefbouraoui1 <bouraouiyoussef12@gmail.com>
2025-05-10 09:23:50 +0100

    adding comething

--- SpaceApp/SpringBackend/FetchingData/src/main/java/net/bouraoui/fetchingdata/Repositories/SatelliteRepository.java
+++ SpaceApp/SpringBackend/FetchingData/src/main/java/net/bouraoui/fetchingdata/Repositories/SatelliteRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface SatelliteRepository extends MongoRepository<Satellite, Integer> {
+public interface SatelliteRepository extends MongoRepository<Satellite,String> {
 
     @Query(value = "{ popular: 'yes' }", sort = "{ popular : -1 }")
     List<Satellite> findTop30ByPopularYes(org.springframework.data.domain.Pageable pageable);
