92bbf70eee680b26bf143d9306b495964ac26fa0
youssefbouraoui1 <bouraouiyoussef12@gmail.com>
2025-05-17 12:33:01 +0100

    healthStatus is saving in the database

---
+++ SpaceApp/SpringBackend/HealthMonitoringService/src/main/java/org/example/healthmonitoringservice/DTOs/HealthStatusDTO.java
@@ -0,0 +1,14 @@
+package org.example.healthmonitoringservice.DTOs;
+
+import lombok.*;
+
+import java.util.Map;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
+public class HealthStatusDTO {
+    private Integer satelliteId;
+    private String satelliteName;
+    private Float prediction;
+    private Float probability;
+    private Map<String, Float> explanation;
+}
--- SpaceApp/SpringBackend/HealthMonitoringService/src/main/java/org/example/healthmonitoringservice/Repositories/HealthStatusRepository.java
+++ SpaceApp/SpringBackend/HealthMonitoringService/src/main/java/org/example/healthmonitoringservice/Repositories/HealthStatusRepository.java
@@ -2,8 +2,12 @@ package org.example.healthmonitoringservice.Repositories;
 
 import org.example.healthmonitoringservice.Entities.HealthStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
 
 public interface HealthStatusRepository extends MongoRepository<HealthStatus, String> {
-
+    @Query(value = "{ 'satelliteId': ?0 }", sort = "{ 'timestamp': -1 }")
+    Optional<HealthStatus> findLatestBySatelliteId(Integer satelliteId);
 }
 
